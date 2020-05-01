6637111affbb3dc95aaa063d8277a088c5ce4b6a
Thor <thor@stripe.com>
2020-05-01 22:12:33 +0800

    Add PRAPI Button

--- src/App.js
+++ src/App.js
@@ -1,24 +1,85 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
+import { useShoppingCart } from 'use-shopping-cart'
+
+import { PaymentRequestButtonElement, useStripe } from '@stripe/react-stripe-js';
+
+const productData = [
+  {
+    name: 'Bananas',
+    sku: 'sku_GBJ2Ep8246qeeT',
+    price: 400,
+    image: 'https://www.fillmurray.com/300/300',
+    currency: 'USD',
+  },
+  {
+    name: 'Tangerines',
+    sku: 'sku_GBJ2WWfMaGNC2Z',
+    price: 100,
+    image: 'https://www.fillmurray.com/300/300',
+    currency: 'USD',
+  },
+]
+
 function App() {
+  const { totalPrice, redirectToCheckout, cartCount, addItem, cartDetails, cartItems } = useShoppingCart()
+
+  const [paymentRequest, setPaymentRequest] = useState(null);
+
+  const stripe = useStripe();
+
+  useEffect(() => {
+    if (stripe && paymentRequest === null) {
+      const pr = stripe.paymentRequest({
+        country: 'US',
+        currency: 'usd',
+        total: {
+          label: 'Demo total',
+          amount: cartItems.reduce((acc, { price }) => acc + price, 0),
+          pending: true
+        },
+        requestPayerName: true,
+        requestPayerEmail: true,
+      });
+      // Check the availability of the Payment Request API first.
+      pr.canMakePayment().then(result => {
+        console.log({ result })
+        if (result) {
+          pr.on('paymentmethod', ev => {
+            console.log({ ev })
+            // POST TO FUNCTION
+            ev.complete('success');
+          })
+          setPaymentRequest(pr);
+        }
+      });
+    } else if (paymentRequest) {
+      paymentRequest.update({
+        total: {
+          label: 'Demo total',
+          amount: cartItems.reduce((acc, { price }) => acc + price, 0),
+        },
+      });
+    }
+  }, [stripe, paymentRequest, cartItems]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {/* Renders the products */}
+      {productData.map((product) => (
+        <button key={product.sku} onClick={() => addItem(product)}>{`Add ${product.name}`}</button>
+      ))}
+      {/* This is where we'll render our cart */}
+      <p>Number of Items: {cartCount}</p>
+      <p>Total: {totalPrice()}</p>
+
+      {paymentRequest ? <PaymentRequestButtonElement options={{ paymentRequest }} /> : ''}
+      {/* Redirects the user to Stripe */}
+      <button onClick={() => redirectToCheckout()}>Checkout</button>
+
+      <pre>{JSON.stringify(cartDetails, null, 2)}</pre>
+      <pre>{JSON.stringify(cartItems, null, 2)}</pre>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -4,10 +4,26 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+import { loadStripe } from '@stripe/stripe-js'
+import { Elements } from '@stripe/react-stripe-js';
+import { CartProvider } from 'use-shopping-cart'
+
+// Remember to add your public Stripe key
+const stripePromise = loadStripe("pk_test_mfWVCJGYkHG8iJPY1IyKOgqn00KN4Nlwwx")
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <CartProvider
+    stripe={stripePromise}
+    successUrl="https://stripe.com"
+    cancelUrl="https://twitter.com/dayhaysoos"
+    currency="USD"
+    allowedCountries={['US', 'GB', 'CA']}
+    billingAddressCollection={true}
+  >
+    <Elements stripe={stripePromise}>
+      <App />
+    </Elements>
+  </CartProvider>,
   document.getElementById('root')
 );
 
